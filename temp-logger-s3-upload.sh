7f69bc93e328423aa9cccf332bdf567975c78c9e
buxibuxi <buxibuxi@gmail.com>
2019-07-15 20:14:43 +0200

    logging into logs dir, s3 sync introduced

--- temp-logger-s3-upload.sh
+++ temp-logger-s3-upload.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 d=`/bin/date +%Y-%m-%d`
-sudo /usr/local/bin/aws s3 cp "/home/pi/temperature-logger-py-aws/temp_rpi2_$d.log" s3://bbaws-temperature/
+sudo /usr/local/bin/aws s3 sync "/home/pi/temperature-logger-py-aws/logs" s3://bbaws-temperature/
